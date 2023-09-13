1fdc37fd4cb62ae9ed67b8cfd21393c3c8737b11
Lukas Berbuer <lukas.berbuer@gmail.com>
2023-09-13 14:58:08 +0200

    refactor: pass spectrum instead of signal

--- band-energy/code.py
+++ band-energy/code.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def band_energy(
-    signal: np.ndarray, samplerate: float, f_lower: float, f_upper: float
+    spectrum: np.ndarray, samplerate: float, f_lower: float, f_upper: float
 ) -> float:
-    powerspectrum = np.abs(np.fft.rfft(signal)) ** 2
+    powerspectrum = np.abs(spectrum) ** 2
     n = len(powerspectrum)
     n_lower = int(2 * n * f_lower / samplerate)
     n_upper = int(2 * n * f_upper / samplerate)
