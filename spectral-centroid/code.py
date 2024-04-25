a93f7ec047593ab2c0d3b38634696fb6fa04f78a
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-04-25 08:56:40 +0200

    fix(spectral-centroid): use complex-valued dft spectrum as input

--- spectral-centroid/code.py
+++ spectral-centroid/code.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def spectral_centroid(spectrum: np.ndarray, samplerate: float):
-    n = len(spectrum)
-    sum_magnitudes = np.sum(spectrum)
-    sum_weighted_magnitudes = np.sum(np.arange(n) * spectrum)
+    magnitudes = np.abs(spectrum)
+    n = len(magnitudes)
+    sum_magnitudes = np.sum(magnitudes)
+    sum_weighted_magnitudes = np.sum(np.arange(n) * magnitudes)
     return 0.5 * samplerate / (n - 1) * sum_weighted_magnitudes / sum_magnitudes
