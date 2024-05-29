4582c7a2567e39abcc2586682a0c792aef031073
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-05-29 14:58:16 +0200

    fix(spectral-rolloff): use power spectrum in code

--- spectral-rolloff/code.py
+++ spectral-rolloff/code.py
@@ -2,11 +2,11 @@ import numpy as np
 
 
 def spectral_rolloff(spectrum: np.ndarray, samplerate: float, rolloff: int):
-    magnitudes = np.abs(spectrum)
-    magnitudes_sum_rolloff = (rolloff / 100) * np.sum(magnitudes)
-    magnitudes_sum = 0.0
-    for i, magnitude in enumerate(magnitudes):
-        magnitudes_sum += magnitude
-        if magnitudes_sum >= magnitudes_sum_rolloff:
-            return 0.5 * samplerate / (len(magnitudes) - 1) * i
+    ps = np.abs(spectrum) ** 2
+    ps_sum_rolloff = (rolloff / 100) * np.sum(ps)
+    ps_sum = 0.0
+    for i, magnitude in enumerate(ps):
+        ps_sum += magnitude
+        if ps_sum >= ps_sum_rolloff:
+            return 0.5 * samplerate / (len(ps) - 1) * i
     return 0.5 * samplerate
